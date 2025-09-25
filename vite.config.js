009ee96c1c3c86de8decee86c9849b39045c9492
angello-hoyos-pascuales <angello201467@gmail.com>
2025-09-25 15:19:25 -0500

    Configuración para despliegue en GitHub Pages: base en vite.config.js, scripts y dependencia gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Configuración para desplegar en GitHub Pages
+// Cambia la base al nombre del repo
 export default defineConfig({
+  base: '/portafolio-angello-hoyos/',
   plugins: [react()],
 })
